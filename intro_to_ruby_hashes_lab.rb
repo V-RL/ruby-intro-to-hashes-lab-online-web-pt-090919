832f557672f2cea74adfafe683237aad326bc146
Brandon Furr <brandon.furr123@gmail.com>
2019-09-27 17:52:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,8 @@ def monopoly
 	      :reading_railroad => {"mortgage_value" => "$100"},
 	      :pennsylvania_railroad => {"mortgage_value" => "$200"},
 	      :b_and_o_railroad => {"mortgage_value" => "$400"},
-	      :shortline => {"mortgage_value" => "$800"},
+	      :shortline => {"mortgage_value" => "$800"}
 	    }
-	  }
 	  }
 	}
 
