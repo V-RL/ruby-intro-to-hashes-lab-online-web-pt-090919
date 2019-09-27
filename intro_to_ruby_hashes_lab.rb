f75d5bf6b7fdcec6e3dfa9bed1f9730f2bf3f809
Brandon Furr <brandon.furr123@gmail.com>
2019-09-27 17:31:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,8 @@ def monopoly
 	  :railroads =>
 	  {
 	    :pieces => 4,
-	    :rent_in_dollars => {}
-	    :names => {}
+	    :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
+	    :names => {},
 	  }
 	}
 
